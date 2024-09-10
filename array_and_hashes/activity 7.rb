bdb48f104f797fc51cdbc5775980f9564465e3ec
Roseanne23 <roseanne.gulosino@gmail.com>
2024-09-10 21:37:27 +0800

    favorite movies length of an array

---
+++ array_and_hashes/activity 7.rb
@@ -0,0 +1,4 @@
+favorite_movies = ["Tangled", "The Hobbit: an unexpected journey", "Charlie and the chocolate factory", "The Notebook", "Inside Out"]
+length_of_array = favorite_movies.length
+print length_of_array
+
