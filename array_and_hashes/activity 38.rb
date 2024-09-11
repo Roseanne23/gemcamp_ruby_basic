9a120ac9f01ccb917d5ee461ca4bc88db9a7314f
Roseanne23 <roseanne.gulosino@gmail.com>
2024-09-11 18:11:52 +0800

    print my birthday

---
+++ array_and_hashes/activity 38.rb
@@ -0,0 +1,7 @@
+number = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  ['-', 0]
+]
+puts "The date of my birth is #{number[0][0]}#{number[2][2]}#{number[2][2]}#{number[0][0]}#{number[3][0]}#{number[0][1]}#{number[0][1]}#{number[3][0]}#{number[3][2]}#{number[2][1]}"
