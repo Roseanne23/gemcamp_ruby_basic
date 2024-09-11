ec415a85bdac1f5941ed6930a3fe518bdcd5c0ff
Roseanne23 <roseanne.gulosino@gmail.com>
2024-09-11 12:00:51 +0800

    print author of your second favorite book

---
+++ array_and_hashes/activity 23.rb
@@ -0,0 +1,12 @@
+favorite_books = [
+  {
+    book:1, title: "The Forever Dog", author: "Karen Shaw Becker"
+  },
+  {
+    book:2, title: "Sailormoon", author: "Nakao Takeuchi"
+  },
+  {
+    book:3, title: "Twilight", author: "Stephenie Meyer"
+  }
+]
+print favorite_books[1]
