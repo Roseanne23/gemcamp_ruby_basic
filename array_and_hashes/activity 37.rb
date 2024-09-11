f2b01025704d010202230b6a2098f1d09a71aee9
Roseanne23 <roseanne.gulosino@gmail.com>
2024-09-11 17:29:05 +0800

    print the name of the 10th student

---
+++ array_and_hashes/activity 37.rb
@@ -0,0 +1,13 @@
+students = [
+  {name: "Kotton", age: 20, grade: 82},
+  {name: "Kati", age: 22, grade: 87},
+  {name: "Lou", age: 25, grade: 90},
+  {name: "Alisae", age: 19, grade: 99},
+  {name: "Henlo", age: 26, grade: 89},
+  {name: "Notnot", age: 24, grade: 91},
+  {name: "Matcha", age: 30, grade: 97},
+  {name: "OP", age: 18, grade: 85},
+  {name: "Blur", age: 21, grade: 88},
+  {name: "Bonk", age: 28, grade: 97}
+  ]
+puts students[9][:name]
