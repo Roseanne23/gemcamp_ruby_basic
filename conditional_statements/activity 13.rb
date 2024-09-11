861b23df4a4b7c62a2ddfa5f2261647cbb611705
Roseanne23 <roseanne.gulosino@gmail.com>
2024-09-11 20:55:02 +0800

    check the numbers if equal or not equal

---
+++ conditional_statements/activity 13.rb
@@ -0,0 +1,7 @@
+puts "Give me a number"
+number_1 = gets.chomp.to_i
+number_2 = gets.chomp.to_i
+if number_1 == number_2
+  print "The numbers are equal"
+  else print "The numbers are not equal"
+end
