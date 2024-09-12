a8a903c4415ecd5c54e81f9f9b3ca00e575eae6e
Roseanne23 <roseanne.gulosino@gmail.com>
2024-09-12 21:06:37 +0800

    input sides of shapes

---
+++ conditional_statements/case/activity 10.rb
@@ -0,0 +1,13 @@
+puts "Input sides of shapes (4,5,6,7):"
+sides = gets.to_i
+
+case sides
+when 4
+  puts "square"
+when 5
+  puts "pentagon"
+when 6
+  puts "hexagon"
+when 7
+  puts "heptagon"
+  end
