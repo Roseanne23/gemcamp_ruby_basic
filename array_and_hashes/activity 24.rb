82c0524480bd5afe9ac460c8494f5b7f9bf4863d
Roseanne23 <roseanne.gulosino@gmail.com>
2024-09-12 09:24:30 +0800

    replace negative numbers to 0

--- array_and_hashes/activity 24.rb
+++
@@ -1,3 +0,0 @@
-key_value = { name: "John", age: 25, city: "New York" }
-key_value[:job] = "developer"
-puts key_value
---
+++ conditional_statements/activity 35.rb
@@ -0,0 +1,10 @@
+negative_numbers = 0
+
+(-20..30).each do |number|
+
+  if number.negative?
+    puts number = 0
+  else
+    puts number
+  end
+end
