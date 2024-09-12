c359d9c8fc60e6e86c2b070e952c3a85e31e6257
Roseanne23 <roseanne.gulosino@gmail.com>
2024-09-12 10:30:30 +0800

    print names longer than 4 characters

--- conditional_statements/activity 39.rb
+++ conditional_statements/activity 39.rb
@@ -1,6 +1,6 @@
-names = [ "hello", "bottle", "rain", "kale", "eat", "sit"]
-names.each do |names|
-  if names.length > 4
-    puts names
+words = [ "hello", "bottle", "rain", "kale", "eat", "sit"]
+words.each do |words|
+  if words.length > 4
+    puts words
   end
 end
