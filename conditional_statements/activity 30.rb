21a9adab18526f3e49df98bd64d9a663712a30cb
Roseanne23 <roseanne.gulosino@gmail.com>
2024-09-12 08:14:42 +0800

    check age if teenager, adult, or not

--- conditional_statements/activity 30.rb
+++ conditional_statements/activity 30.rb
@@ -7,9 +7,3 @@ elsif age >= 20
 else age < 13
 print " You are a child "
 end
-
-
-
-#Write a program that asks for a person's age and checks if they are a teenager (between 13 and 19)
-#or an adult (20 and older). If they are a teenager, print You are a teenager.
-#If they are an adult, print You are an adult. If they are younger than 13, print You are a child.
